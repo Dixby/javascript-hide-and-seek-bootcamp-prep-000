9c67997cecaba2a09fe78363ea9c505bb814800a
Andrew Walker <hovercar00@gmail.com>
2018-01-11 16:54:12 +0000

    Done.

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector(`#nested .target`);
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
 }
 
 function increaseRankBy(n) {
- const rankedListContents = document.querySelectorAll('.ranked-list');
-  for (let i = 0, l = rankedListContents.length; i < l; i++) {
-    let children = rankedListContents[i].children;
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  
+ for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
     
     for (let j = 0, k = children.length; j < k; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n;
     }
   }
 }
-
-function deepestChild() {
-  const dchild = document.querySelector('#grand-node div div div div');
-  return dchild;
- } 
